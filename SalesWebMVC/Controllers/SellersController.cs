e05efcfb93817ea29a187ffcd307ccadd996df4d
cirosouza <ciro_lut@hotmail.com>
2021-05-08 12:21:16 -0300

    foreign key not null (referential integrity)

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -3,12 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+<<<<<<< HEAD
 using SalesWebMVC.Services;
+=======
+>>>>>>> SellersController
 
 namespace SalesWebMVC.Controllers
 {
     public class SellersController : Controller
     {
+<<<<<<< HEAD
         private readonly SellerService _sellerService;
 
         public SellersController(SellerService sellerService)
@@ -19,6 +23,11 @@ namespace SalesWebMVC.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+=======
+        public IActionResult Index()
+        {
+            return View();
+>>>>>>> SellersController
         }
     }
 }
