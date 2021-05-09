fc09d0f056c6c0b3a949e192f7533d2b9b5c71b8
cirosouza <ciro_lut@hotmail.com>
2021-05-09 18:25:09 -0300

    SellerFormViewModel and Department select component

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -1,33 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-<<<<<<< HEAD
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+using SalesWebMVC.Models.ViewModels;
 using SalesWebMVC.Services;
-=======
->>>>>>> SellersController
 
-namespace SalesWebMVC.Controllers
+namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
-<<<<<<< HEAD
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService)
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
+
         public IActionResult Index()
         {
             var list = _sellerService.FindAll();
             return View(list);
-=======
-        public IActionResult Index()
+        }
+
+        public IActionResult Create()
         {
-            return View();
->>>>>>> SellersController
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ SalesWebMVC/Models/ViewModels/SellerFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Models.ViewModels
+{
+    public class SellerFormViewModel
+    {
+        public Seller Seller { get; set; }
+        public ICollection<Department> Departments { get; set; }
+    }
+}
